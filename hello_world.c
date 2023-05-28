39c2171746ca66b0b3a157eaff19ec2d83e33446
Arun S Nair <111906073+z-arun@users.noreply.github.com>
2023-05-28 16:39:31 +0530

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,1 +1,19 @@
-/
+#include<linux/module.h>
+#include<linux/init.h>
+#include<linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+
+int __init startFun(void)
+{
+	printk(KERN_INFO "Hello world");
+	return 0;
+}
+
+void __exit endFun(void)
+{
+	printk(KERN_INFO "bye world");
+}
+
+module_init(startFun);
+module_exit(endFun);
